0a37723d9d3706d8f21d0b726f761e049a3ec11b
yurienik <83026410+yurienik@users.noreply.github.com>
2025-03-20 18:42:12 +0000

    Pastas Python

---
+++ Cod.py/Maior_Menor.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+
+if a > b:
+    print("A maior que b")
+elif b > a:
+    print("B maior que A")
---
+++ Maior_Menor.c
@@ -0,0 +1,43 @@
+//Bliblioteca
+#include <stdio.h>
+
+int main ()
+{
+    int n1;
+    int n2;
+    int n3;
+
+    n1 = printf("Insira o primeiro número\n");
+    scanf("%d", &n1);    
+
+    n2 = printf("Insira o segundo número\n");
+    scanf("%d", &n2);
+
+    n3 = printf("Insira o terceiro número\n");
+    scanf("%d", &n3);
+
+    if (n1 < n2) {
+        if (n2 < n3) {
+            printf("%d %d %d", n3, n2, n1)            
+            };
+        else {
+            if (n1 < n3){
+                printf("%d %d %d", n1, n3, n2)
+            }
+            else {
+                printf("%d %d %d", n3, n1, n2)
+    else {
+        if(a < c)
+        printf("%d %d %d", n2, n1, n3)
+        else {
+            if (n2 < n3) {
+                printf("%d %d %d", n2, n3, n1)
+            else (
+                printf("%d %d %d", n3, n2, n1)
+            )
+            }
+        }
+            
+    }
+ return 0;
+}
